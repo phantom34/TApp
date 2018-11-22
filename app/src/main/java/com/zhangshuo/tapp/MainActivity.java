f7046fd49d2fa6270b12f0c747c760b01deced99
zhangshuo <fastshare@163.com>
2018-11-22 14:24:25 +0800

    first commit

---
+++ app/src/main/java/com/zhangshuo/tapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zhangshuo.tapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
